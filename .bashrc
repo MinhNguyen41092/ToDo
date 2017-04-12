67c16dbecf7ad31d7d0f40365639c4cc3ec7cbd7
Your Name <forever.alone92@gmail.com>
2017-04-12 09:26:54 +0700

    Email confirmation using Gmail

---
+++ .bashrc
@@ -0,0 +1,4 @@
+# Sending email confirmation variables for ToDo app
+export  SENDMAIL_PASSWORD=Dinhminh788758  
+export  SENDMAIL_USERNAME=forever.alone92@gmail.com
+export  MAIL_HOST=localhost:3000
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -52,7 +52,16 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
   
-  config.action_mailer.default_url_options = { host: 'localhost:3000' }
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {:address => "localhost", :port => 1025}
+  config.action_mailer.smtp_settings = {
+    user_name:      'forever.alone92@gmail.com',
+    password:       'Dinhminh788758',
+    domain:         'gmail.com',
+    address:        'smtp.gmail.com',
+    port:           '587',
+    authentication: :plain,
+    enable_starttls_auto: true    }
 end
