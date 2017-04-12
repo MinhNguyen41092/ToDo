de854448d643639ba315359b60a061ac5b9c310f
Your Name <forever.alone92@gmail.com>
2017-04-12 09:52:41 +0700

    Heroku mail confirmation

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -56,12 +56,5 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    user_name:      'forever.alone92@gmail.com',
-    password:       'Dinhminh788758',
-    domain:         'gmail.com',
-    address:        'smtp.gmail.com',
-    port:           '587',
-    authentication: :plain,
-    enable_starttls_auto: true    }
+  
 end
